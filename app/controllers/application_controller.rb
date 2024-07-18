4237967e0fe12bc0b273a6d3a5cd4c80a56e5b28
steffi <steffi.amper@gmail.com>
2024-07-17 22:30:28 -0500

    fix deleted main categories

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_main_categories
 
-  protected
+  private
+
+  def set_main_categories
+    @main_categories = Category.where(sub_category_id: nil)
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :province_id, customer_address_attributes: [:address]])
